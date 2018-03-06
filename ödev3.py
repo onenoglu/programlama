3c1d5fd8d2458e6943f408c9e28086957d403fe5
onenoglu <36936135+onenoglu@users.noreply.github.com>
2018-03-07 02:20:01 +0300

    Add files via upload

---
+++ ödev3.py
@@ -0,0 +1,8 @@
+def adambasiciro():
+    x=int(input("Satış Miktarı:"))
+    y=int(input("Birim Satış Fiyatı:"))
+    ciro=x*y
+    print("Ciro:",ciro)
+    z=int(input("Toplam Çalışan Sayısı:"))
+    adambasiciro=ciro/z
+    print("Adam Başı Ciro:",adambasiciro)
